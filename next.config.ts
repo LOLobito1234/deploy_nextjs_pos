0aa293463208836b1c333b2d9230bdebf44faa92
Jorge123129 <jorgitoeltroll@gmail.com>
2025-11-11 13:06:28 -0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'deploy-pos-nestjs.onrender.com', 'res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: "deploy-pos-nestjs-x6dn.onrender.com",
-        port: '',
-        pathname: '/assets/**'
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/image/**'
       }
     ]
   }
